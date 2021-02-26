fa9bfa6a9ebfe14889e095da8d9c8c5809845ee3
Marie-Jeanne Brousseau <mariejeanne@classcraft.com>
2021-02-25 22:28:16 -0500

    Exercise 05 complete

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -12,9 +12,50 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+const smallBox = (
+  // <div
+  //   className="box box--small"
+  //   style={{fontStyle: 'italic', backgroundColor: 'lightblue'}}
+  // >
+  //   small lightblue box
+  // </div>
+  <Box size="small" style={{backgroundColor: 'lightblue'}}>
+    small lightblue box
+  </Box>
+)
+const mediumBox = (
+  // <div
+  //   className="box box--medium"
+  //   style={{fontStyle: 'italic', backgroundColor: 'pink'}}
+  // >
+  //   medium pink box
+  // </div>
+  <Box size="medium" style={{backgroundColor: 'pink'}}>
+    medium pink box
+  </Box>
+)
+const largeBox = (
+  // <div
+  //   className="box box--large"
+  //   style={{fontStyle: 'italic', backgroundColor: 'orange'}}
+  // >
+  //   large orange box
+  // </div>
+  <Box size="large" style={{backgroundColor: 'orange'}}>
+    large orange box
+  </Box>
+)
+
+function Box({size, className = '', style, ...otherProps}) {
+  const sizeClassName = size ? `box--${size}` : ''
+  return (
+    <div
+      className={`box ${className} ${sizeClassName}`.trim()}
+      style={{fontStyle: 'italic', ...style}}
+      {...otherProps}
+    />
+  )
+}
 
 function App() {
   return (
